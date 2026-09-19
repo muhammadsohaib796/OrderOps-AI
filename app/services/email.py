f98bf75eff936515d6858a4c6d78ab2eb54bc129
muhammadsohaib796 <sohaibshakeel796@gmail.com>
2026-09-19 21:05:22 +0500

    Use BASE_URL env var instead of hardcoded localhost in email links

--- app/services/email.py
+++ app/services/email.py
@@ -8,6 +8,8 @@ load_dotenv()
 configuration = sib_api_v3_sdk.Configuration()
 configuration.api_key["api-key"] = os.getenv("BREVO_API_KEY")
 
+BASE_URL = os.getenv("BASE_URL", "http://127.0.0.1:8000")
+
 
 def send_negotiation_email(to_email: str, customer_name: str, offers: list[dict]) -> bool:
     api_instance = sib_api_v3_sdk.TransactionalEmailsApi(sib_api_v3_sdk.ApiClient(configuration))
@@ -23,9 +25,9 @@ def send_negotiation_email(to_email: str, customer_name: str, offers: list[dict]
     <p>Hi {customer_name},</p>
     <p>Part of your order needs your input:</p>
     <ul>{offer_lines}</ul>
-    <p><a href="http://127.0.0.1:8000/respond?order_id={offers[0]['order_id']}&decision=accept">Accept alternative</a>
-       | <a href="http://127.0.0.1:8000/respond?order_id={offers[0]['order_id']}&decision=decline">Decline, refund instead</a></p>
-    """
+     <p><a href="{BASE_URL}/respond?order_id={offers[0]['order_id']}&decision=accept">Accept alternative</a>
+    | <a href="{BASE_URL}/respond?order_id={offers[0]['order_id']}&decision=decline">Decline, refund instead</a></p>"""
+
 
     email = sib_api_v3_sdk.SendSmtpEmail(
         to=[{"email": to_email, "name": customer_name}],
